4c2b2c5f894b3eb36f9cd62998e3cb1bb509096d
Guido Matos Camones <guido.matos.88@gmail.com>
2020-08-08 23:58:03 -0500

    Creación de proyecto MVC

---
+++ LibraryMvcApp/Data/Services/IBookService.cs
@@ -0,0 +1,16 @@
+﻿using LibraryMvcApp.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryMvcApp.Data.Services
+{
+    public interface IBookService
+    {
+        IEnumerable<Book> GetAll();
+        Book Add(Book newBook);
+        Book GetById(Guid id);
+        void Remove(Guid id);
+    }
+}
