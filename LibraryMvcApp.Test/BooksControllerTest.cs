5d1d4551ec5e9a9e0e5204b4e56ef632cb7a2b2a
Guido Matos Camones <guido.matos.88@gmail.com>
2020-08-09 11:45:52 -0500

    Unit testing details ActionResult

--- LibraryMvcApp.Test/BooksControllerTest.cs
+++ LibraryMvcApp.Test/BooksControllerTest.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Moq;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace LibraryMvcApp.Test
@@ -26,6 +27,36 @@ namespace LibraryMvcApp.Test
             var viewResult = Assert.IsType<ViewResult>(result);
             var viewResultBooks = Assert.IsAssignableFrom<List<Book>>(viewResult.ViewData.Model);
             Assert.Equal(5, viewResultBooks.Count);
+        }
+
+        [Theory]
+        [InlineData("ab2bd817-98cd-4cf3-a80a-53ea0cd9c200", "ab2bd817-98cd-4cf3-a80a-53ea0cd9c123")]
+        public void DetailsUnitTest(string validGuid, string invalidGuid) {
+            //arrange
+            var mockRepo = new Mock<IBookService>();
+            var validItemGuid = new Guid(validGuid);
+            mockRepo.Setup(n => n.GetById(validItemGuid)).Returns(MockData.GetTestBookItems().FirstOrDefault(x => x.Id == validItemGuid));
+            var controller = new BooksController(mockRepo.Object);
+
+            //act
+            var result = controller.Details(validItemGuid);
+
+            //assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var viewResultValue = Assert.IsAssignableFrom<Book>(viewResult.ViewData.Model);
+            Assert.Equal("Managing Oneself", viewResultValue.Title);
+            Assert.Equal("Peter Drucker", viewResultValue.Author);
+            Assert.Equal(validItemGuid, viewResultValue.Id);
+
+            //arrange
+            var invalidItemGuid = new Guid(invalidGuid);
+            mockRepo.Setup(n => n.GetById(invalidItemGuid)).Returns(MockData.GetTestBookItems().FirstOrDefault(x => x.Id == invalidItemGuid));
+
+            //act
+            var notFoundResult = controller.Details(invalidItemGuid);
+
+            //assert
+            Assert.IsType<NotFoundResult>(notFoundResult);
         }
     }
 }
